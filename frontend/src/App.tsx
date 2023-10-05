a1609aab3f25f735e47b7af056aca221413f009d
Koya <44787464+koyaboy@users.noreply.github.com>
2023-10-05 11:54:32 +0100

    Static Comment Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Comment from "./components/Comment"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-very-light-gray min-h-screen py-8 px-4">
+      <Comment />
+    </div>
   )
 }
